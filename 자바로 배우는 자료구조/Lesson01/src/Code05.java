5c15fa5490765457eae74272e16bdd25d6996503
DESKTOP-O5UAK6C\user <k.do.dodong@gmail.com>
2021-12-03 16:12:02 +0900

    1-3장 변수, 배열, 반복문(3) - 배열

---
+++ 자바로 배우는 자료구조/Lesson01/src/Code05.java
@@ -0,0 +1,27 @@
+
+public class Code05 {
+
+	public static void main(String[] args) {
+		
+		// in C, int grades[5];
+		
+		int [] grades;
+		
+		grades = new int[5];
+		
+		
+		// assign some values to the array
+		grades[0] = 100;
+		grades[1] = 76;
+		grades[2] = 92;
+		grades[3] = 95;
+		grades[4] = 14;
+		
+		//print out each value
+		System.out.println( grades[0] );
+		System.out.println( grades[1] );
+		System.out.println( grades[2] );
+		System.out.println( grades[3] );
+		System.out.println( grades[4] );
+	}
+}
